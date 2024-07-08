1143ebe2174243322e34d66a47b6cfcb65aea613
yhn-dm <yohann2003@hotmail.fr>
2024-07-08 22:47:54 +0200

    Ajout du projet

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { makeStyles } from "@material-ui/core";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
